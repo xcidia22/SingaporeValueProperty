0bdd799f73041e88f1ebab3299504a492e5c8013
xcidia22 <35554378+xcidia22@users.noreply.github.com>
2021-06-13 20:11:43 +0800

    Origin

---
+++ assets/js/owl.custom.js
@@ -0,0 +1,265 @@
+ $(document).ready(function(){
+    $('.owl-one').owlCarousel({
+        nav:true,
+        navSpeed:true,
+        rewind:true, 
+        margin:27,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplay:true,
+        autoplayTimeout:8000,
+        autoplayHoverPause:true,
+        responsive:{
+
+           0:{
+                items:1
+            },
+
+           480:{
+                items:1
+            },
+
+            640:{
+                items:2
+            },
+
+            990:{
+                items:3
+            },
+
+            1140:{
+                items:4
+            }
+        }
+    });
+
+     $('.owl-one1').owlCarousel({
+        loop:true, 
+        margin:27,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplay:true,
+        autoplayTimeout:7000,
+        autoplayHoverPause:true,
+        responsive:{
+
+           0:{
+                items:1
+            },
+
+           480:{
+                items:1
+            },
+
+            640:{
+                items:2
+            },
+
+            990:{
+                items:3
+            },
+
+            1140:{
+                items:4
+            }
+        }
+    });
+
+    $('.owl-two').owlCarousel({
+        loop:true,
+        margin:30,
+        responsive:{
+           0:{
+                items:1
+            },
+
+            480:{
+                items:1
+            },
+            640:{
+               items:2
+            },
+            800:{
+                items:3
+            },
+
+            1140:{
+                items:3
+            }
+        }
+    });
+
+    $('.owl-three').owlCarousel({
+        loop:true,
+        margin:20,
+        responsive:{
+           0:{
+                items:1
+            },
+
+            400:{
+                items:2
+            },
+
+            640:{
+                items:4
+            },
+
+            800:{
+                items:5
+            },
+            1000:{
+                items:6
+            }
+        }
+    });
+
+     $('.owl-four').owlCarousel({
+        loop:true,
+        margin:28,
+        responsive:{
+           0:{
+                items:1
+            },
+
+           480:{
+                items:1
+            },
+
+            640:{
+                items:1
+            },
+
+            768:{
+                items:2
+            },
+
+            990:{
+                items:2
+            },
+
+            1140:{
+                items:2
+            }
+        }
+    });
+
+     $('.owl-five').owlCarousel({
+        loop:true,
+        margin:30,
+        responsive:{
+           0:{
+                items:1
+            },
+
+          
+            480:{
+                items:1
+            },
+
+
+            640:{
+                items:2
+            },
+            768:{
+                items:2
+            },
+
+            1140:{
+                items:3
+            }
+        }
+    });
+   
+});
+
+ $('.owl-six').owlCarousel({
+        loop:true,
+        margin:20,
+        responsive:{
+           0:{
+                items:1
+            },
+
+            400:{
+                items:2
+            },
+
+            640:{
+                items:4
+            },
+
+            800:{
+                items:4
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+
+  $('.owl-seven').owlCarousel({
+        nav:true,
+        navSpeed:true,
+        rewind:true, 
+        margin:27,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplay:true,
+        autoplayTimeout:8000,
+        autoplayHoverPause:true,
+        responsive:{
+            320:{
+                items:1
+            }
+            ,
+           1000:{
+                items:1
+            }
+        }
+    });
+
+
+  $('.owl-main').owlCarousel({
+        loop:true,
+        margin:0,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplay:true,
+        autoplayTimeout:8000,
+        autoplayHoverPause:false,
+        responsive:{
+            320:{
+                items:1
+            }
+            ,
+           1920:{
+                items:1
+            }
+        }
+    });
+
+
+
+  $('.owl-preview').owlCarousel({
+        loop:true,
+        margin:0,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplay:true,
+        autoplayTimeout:7000,
+        autoplayHoverPause:false,
+        responsive:{
+            320:{
+                items:1
+            }
+            ,
+           1920:{
+                items:1
+            }
+        }
+    });
+
+
+  
